a5df12934574222f9361d569ba49bab21d523cfd
Unknown <Andrea.m.salazar@me.com>
2017-09-20 19:44:22 -0600

    Added the 6 chances
    in the discription

--- Game/Assets/DGM1600.cs
+++ Game/Assets/DGM1600.cs
@@ -23,22 +23,25 @@ public class DGM1600 : MonoBehaviour {
 
 		textBox.text = "Welcome to Number Guesser "
 		+ "\npick a number in your head"
+		+ "\n\nI have 6 chances to guess your number"
 		+ "\n\nThe Highest number you can pick is " + max
 		+ "\nThe lowest number you can pick is " + min
 		+ "\n\nIs the number higher or lower than " + guess
 		+ "\nUp arrow for higher, Down for lower, Enter for equal";
+
 		  
-
-
 		print ("Welcome To Number Guesser");
 		print ("Pick a number in your head");
+		print ("I have 6 chances to guess your number");
 
 		print ("The Highest number you can pick is " + max);
 		print ("The lowest number you can pick is " + min);
 
 		print ("Is the number higher or lower than " + guess);
 		print ("Up arrow for higher, Down for lower, Enter for equal");
+	
 		max = max + 1;
+
 	}
 	    // Update is called once per frame 
 	void Update ()
@@ -52,8 +55,8 @@ public class DGM1600 : MonoBehaviour {
 			if (Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.DownArrow)) 
 			{
 				//counter--;
-				print ("I Win!");
-				textBox.text = "\n I Win!";
+				print ("You Win!");
+				textBox.text = "\n You Win!";
 			}
 		}
 		else if (Input.GetKeyDown(KeyCode.UpArrow))
@@ -61,21 +64,21 @@ public class DGM1600 : MonoBehaviour {
 			min = guess;
 			guess = (max + min) / 2;
 			counter--;
-			print ("is the number higher or lower than" + guess);
-			textBox.text = "is the number higher or lower than " + guess;
+			print ("is the number equal to higher or lower than" + guess);
+			textBox.text = "is the number equal to higher or lower than " + guess;
 		}
 		else if (Input.GetKeyDown (KeyCode.DownArrow)) 
 		{
 			max = guess;
 			guess = (max + min) /2;
 			counter--;
-			print ("Is the number higher or lower than " + guess);
-			textBox.text = "is the number higer or lower than " + guess;
+			print ("Is the number equal to higher or lower than " + guess);
+			textBox.text = "is the number equal to higher or lower than " + guess;
 		}
 	    if (Input.GetKeyUp (KeyCode.Return)) 
 		{
-			print ("You Win, I lose");
-			textBox.text = "You Win, I lose";
+			print ("I Win, You lose");
+			textBox.text = "I Win, You lose";
 	    }
 
 		if (counter == 0)
